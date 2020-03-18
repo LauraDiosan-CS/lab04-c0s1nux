fafe32aae875c017d207729696d536407470a54a
c0s1nux <siritanu90@gmail.com>
2020-03-18 10:25:31 +0200

    Lab4 only Live requirements met.

---
+++ problemLab4/problemLab4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "repositoryTests.h"
+using namespace std;
+
+int main()
+{
+    cout << "Starting the app!\n";
+    testRepository();
+    cout << "App is still WIP! ( Only Live requirements are met! )\n";
+    cout << "-------------------------------------------------------------\n";
+    return 0;
+}
